87f086e1951a4093123775946e5d85a5d8843936
KIMDONGHYUN-rgb <s010314@naver.com>
2024-07-17 17:14:35 +0900

    20240717

---
+++ first_project/src/first_project/Hello.java
@@ -0,0 +1,17 @@
+package first_project;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("첫번쨰 코드!");
+		// System.out.println("두번쨰 코드!");
+		System.out.println("세번쨰 코드!"); // 세번쨰 출력
+		System.out.println("네번쨰 코드!");
+		
+		/* System.out.println("다섯번쨰 코드!");
+		System.out.println("여섯번쨰 코드!"); */
+		System.out.println(/*"일곱번쨰 코드!"*/"여덟번쨰 코드!");
+	}
+
+}
